e8c5c5c4080556031873f7add3c29c52057065f1
crassolini <crassolini@yahoo.com.br>
2022-07-12 15:09:44 -0300

    Exception handling - findById

---
+++ src/main/java/localhost/curso/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package localhost.curso.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Long id) {
+		super("Resource not found. Id " + id);
+	}
+}
